5f3fd456a28588f2854945b069b0a62ecf77c8cc
maiya-22 <maiya_o@yahoo.com>
2019-01-29 14:31:15 -0500

    Readme updated. Comments edited.

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+The Goal of this App:
+ is to simplify react-redux onto one page of code (in the App.js file) 
+ so that it's possible to see all of the different pieces in one place.
+
+ In the app.js, data is fetched asynchronistically and used to update the App's state in the Provider/Store.
+
+INSTRUCTIONS:
+
+install:
+npm install
+
 start app:
 npm start
 
-Turn off "prettier" extension, so that code does not reformat on save.
+Turn off "prettier" extension, so that code does not reformat on save. 
+It's written to try to make things more clear/ uses few arrow functions, etc.
 
-make sure to be on Chrome browser, with Redux extension installed.
+Make sure to be on Chrome browser, with Redux extension installed.
--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import thunk from 'redux-thunk'
 import PropTypes from 'prop-types';
 
 
-// simulating data objects stored on the backend:
+// simulating data objects stored on a server on the backend:
 const localhost = {
   "/video/0": { title: "Coffee in the Morning", src: "./videos/Coffee-in-the-Morning.mp4" },
   "/video/1": { title: "California Beach", src: "./videos/California-Beach.mp4" }
 }
 
 
-// simulate axios request to backend/ returns a promise with the data:
+// simulate axios request to server/ returns a promise with the data:
 const axios = {
   get: function (url) {
     return new Promise(function (resolve, reject) {
@@ -27,7 +27,7 @@ const axios = {
   }
 }
 
-// create an object to hold the initial state:
+// create an object to hold the initial state of the App:
 let initialState = {
   video: { title: "Coffee in the Morning", src: "./videos/Coffee-in-the-Morning.mp4" },
   isLoggedIn: false,
@@ -36,7 +36,7 @@ let initialState = {
 };
 
 
-// TYPES: These consts will eventually replace the strings in the reducer, so that you don't accidentally name two different actions the same thing and debugging:
+// TYPES: These consts will eventually replace the strings in the reducer, so that you don't accidentally name two different actions the same thing and debugging because if you spell a variable wrong, you get an error, but if you spell a string wrong, you do not.
 
 const actionTypes = {
   VIDEO_LOADING: "VIDEO_LOADING"
@@ -44,9 +44,8 @@ const actionTypes = {
 
 // reduce means to take different things and combine them into one thing.
 //you are taking the previous state object and the properties that are on the action object, and putting them together on a new object and returning that new object.
-let reducer = function (state = initialState, action) {
-  // state object will be passed by the Provider and
-  //action = {type: "ACTION_NAME"}
+let reducer = function (state = initialState, action) { //action = {type: "ACTION_NAME"}
+  // state object will be passed by the Provider
   switch (action.type) {
     case "VIDEO_LOADING":
       return {
@@ -55,14 +54,12 @@ let reducer = function (state = initialState, action) {
         videoError: null
       };
     case "LOAD_VIDEO_SUCCESS":
-      // console.log("VIDEO SUCCESS in reducer")
       return {
         ...state,
         videoIsLoading: false,
         video: action.payload,
         videoError: null
       };
-    // store.dispatch({ type: "VIDEO_LOAD_ERROR", error: { message: "cant find video" } })
     case "VIDEO_LOAD_ERROR":
       return {
         ...state,
@@ -75,19 +72,19 @@ let reducer = function (state = initialState, action) {
       return state;
   }
 };
+
 // import {combineReducers } from "redux";
 // const allReducers = combineReducers({ reducer: reducer });
-
 // PUT ALL THIS IN THE store.js and simply export the finished store:
 // the store is the place where your state is stored, and where the reducer function acts on it, in order to create a new state, when it is updated
 // import { createStore } from "redux";
 // import { applyMiddleware } from "redux";
 // import { compose } from "redux";
 // import thunk from 'redux-thunk'
-// not sure why you use thunk. I though it was so that store would respond to async dispatches, but app seems to do that without it.
+// I think thunk makes the Provider receive async dispatches.
 const middleware = [thunk];
 const store = createStore(reducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
-
+// third param above is for the redux chrome extension
 
 class Video extends Component {
   constructor(props) {
@@ -109,7 +106,7 @@ class Video extends Component {
           <div>{this.props.videoIsLoading ? "Next video is loading ..." : ""}</div>
           <video src={this.props.video ? this.props.video.src : ""} controls />
         </div>
-        {/* hard-coding for now, but eventually get info from the event object that the dom will pass to the event listener */}
+        {/* hard-coding for now, but in reall app, get info from the event object that the dom will pass to the event listener */}
         <button onClick={(event) => { this.changeVideo("/video/1") }}>load video with id: 1</button>
         <button onClick={(event) => { this.changeVideo("/video/0") }}>load video with id: 0</button>
         <button onClick={(event) => { this.changeVideo("/video/zonk") }}>load video with id that is not there</button>
@@ -128,7 +125,7 @@ function mapStateToProps(state) {
   };
 }
 
-// these functions simply return an object
+// these functions simply return an object, nothing else:
 const actionCreators = {
   loadVideoSuccess: function (url) {
     // logic to format action
@@ -142,12 +139,11 @@ const actionCreators = {
     return {
       type: "VIDEO_LOAD_ERROR", error: { message: "cant find video" }
     }
-  }, loadVideoAsync: function (url) {
-    console.log("URL:", url)
   }
-
-
 }
+
+// this function returns an object. It has a dispatch parameter. The Provider will pass the store.dispatch as the argument for the dispatch parameter.
+// each property on the object returns a function. Those functions fire the dispatch on the actions, which are created from the functions on the actionCreators object.
 function matchDispatchToProps(dispatch) {
   return {
     loadVideoSync: function (url) {
@@ -159,6 +155,7 @@ function matchDispatchToProps(dispatch) {
     },
     loadVideoAsync: function (url) {
       dispatch({ type: "VIDEO_LOADING" })
+      // the setTimeout is being used to simulate the time it could take to fetch from a server.
       setTimeout(function () {
         axios.get(url).then(response => {
           console.log("RESPONSE:", response)
@@ -178,8 +175,8 @@ Video = connect(mapStateToProps, matchDispatchToProps)(Video)
 
 class App extends Component {
   render() {
-    // import { Provider } from "react-redux";
     return (
+      // import { Provider } from "react-redux";
       <Provider store={store}>
         <div className="App">
           <h1>Practice App</h1>
